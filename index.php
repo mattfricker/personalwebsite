908a39d777521e47e3377dad0abf2aa004ac5c5f
Matthew Fricker <matt.s.fricker@gmail.com>
2015-03-31 18:57:50 -0400

    Creating the original repo

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Matt Fricker</title>
+    <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,300%7CArvo:400,700'>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <meta name="viewport" content="width=device-width">
+  </head>
+  
+  <body> 
+    <div id="main-content">
+        <div id="large-header">
+            <section class="float-left group hidden" id="information-box">
+                <button class='float-right text-white' id="close-information-box">Close</button>
+            </section>
+            <div id="beliefs">
+                <p class="border-bottom-thin">Do cool stuff.</p>
+                <p class="border-bottom-thin">Learn every day.</p>
+                <p class="border-bottom-thin">Be generous.</p>
+            </div>
+        </div>
+    </div>
+
+    <section id="secondary-content">
+        <div class="float-left more-info group border-right" id="web-dev" data-info="webdev">
+            <h2><a href="statics/webdev.html">Web Development</a></h2>
+        </div>
+        <div class="float-left more-info group border-right" id="education" data-info="education">
+            <h2><a href="statics/education.html">Education</a></h2>
+        </div>
+        <div class="float-left more-info group" data-info="securitystudies">
+            <h2><a href="statics/securitystudies.html">Security Studies</a></h2>
+        </div>
+    </section>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/homepage.js"></script>
+
+  </body>
+
+</html>
---
+++ js/homepage.js
@@ -0,0 +1,77 @@
+var homePage = {
+    initialLoadScreen: $('<div></div>'),
+    spinner: $('<div></div>'),
+    headlineList: $('#beliefs').find('p'),
+    moreInfos: $('.more-info'),
+    mainInfoBox: $('#information-box'),
+    closeMainBox: $('#close-information-box'),
+    allClickers: $('#secondary-content').find('div'),
+    clickerLinks: $('#secondary-content').find('a'),
+    init: function(){
+        this.headlineList.addClass('out-of-sight');
+        this.headlineList.removeClass('border-bottom-thin');
+        this.moreInfos.hide();
+        $('body').append(this.initialLoadScreen);
+        this.initialLoadScreen.addClass('initial-load-screen');
+        this.initialLoadScreen.append(this.spinner);
+        this.spinner.addClass('loading-spinner');
+    },
+    showMoreInfos: function(){
+        this.moreInfos.first().fadeIn(2000, function(){
+            $(this).next().fadeIn(1233, function(){
+                $(this).next().fadeIn(1233);
+            });
+        });
+    },
+    showBeliefs: function(){
+        this.headlineList.last().animate({
+            top: '200px'
+        }, 1700, 'easeOutCubic', function(){
+            $(this).addClass('horizontal-borders-thin')
+                .animate({
+                    top: '0px'
+                }, 1000, 'easeOutCubic', function(){
+                    $(this).prev().animate({
+                        top: '0px'
+                    }, 900, 'easeOutBounce', function(){
+                        $(this).addClass('top-border-thin')
+                            .prev().animate({
+                                top: '0px'
+                            }, 1100, 'easeOutCubic')
+                    })
+                });
+        });
+    }
+};
+
+jQuery(document).ready(function() {
+    homePage.init();
+    homePage.initialLoadScreen.fadeOut(3000, function(){
+        homePage.showBeliefs();
+        homePage.showMoreInfos();
+    });
+
+    homePage.clickerLinks.attr('href','#main-content');
+
+    homePage.allClickers.on('click', function(){
+        homePage.allClickers.removeClass('information-box-color');
+        $(this).addClass('information-box-color');
+        var clickedButton = $(this).attr('data-info');
+        homePage.mainInfoBox.removeClass('hidden').find('.more-infos').remove();
+        homePage.headlineList.parent().addClass('mobile-hide');
+        $.ajax('ajaxcalls/' + clickedButton + '.html',{
+            success: function(response){
+                homePage.mainInfoBox.append(response);
+            },
+            error: function(request, errorType, errorMessage){
+                alert('Error: ' + errorType + ' with message: ' + errorMessage);
+            },
+        });
+    });
+
+    homePage.closeMainBox.on('click', function(){
+        homePage.mainInfoBox.addClass('hidden');
+        homePage.allClickers.removeClass('information-box-color');
+    });
+
+});
